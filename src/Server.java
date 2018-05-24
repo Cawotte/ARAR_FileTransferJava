ee7c310f0868f37f4cbd89f7394019cc1d6b122a
Fab <fab.mckay@gmail.com>
2018-05-24 09:59:06 +0200

    ALLO LE MONDE

--- src/Server.java
+++ src/Server.java
@@ -12,29 +12,35 @@ public class Server
             ServerSocket serverSocket = new ServerSocket(80);
             while(true)
             {
+                System.out.println("Waiting new Request ...");
+
+                //Reception de la requête
                 Socket clientSocket = serverSocket.accept();
                 InputStreamReader inputReader = new InputStreamReader(clientSocket.getInputStream());
                 BufferedReader reader = new BufferedReader(inputReader);
 
+                //on récupère la première ligne c'est celle qui contient la méthode
                 String httpRequest = reader.readLine();
 
+                //On la sépare pour analyser la méthode
                 String[] splitRequest = httpRequest.split(" ");
 
+                //Si c'est un GET
                 if(splitRequest[0].equals("GET"))
                 {
                     System.out.println("C'est un GET");
                     System.out.println("Il veut " + splitRequest[1]);
                 }
 
-
                 String line = reader.readLine();
-
 
                 while(!line.isEmpty())
                 {
                     line = reader.readLine();
                 }
 
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + "Reçu Reçu";
+                clientSocket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
             }
         }
         catch(Exception e)
