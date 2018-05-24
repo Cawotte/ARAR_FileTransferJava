85160208493ae06b5aa6cae48e4396c3353ad7db
Cawotte <khamaz.ee@gmail.com>
2018-05-24 09:58:46 +0200

    Client boucle envoie msg et reception msg simples

--- src/Client.java
+++ src/Client.java
@@ -25,55 +25,54 @@ public class Client
         InetAddress serveurAdress;
 
         //"http://192.168.43.4/";
+        //Adresse fabien : "192.168.43.4"
         String nomAdresseServeur = "192.168.43.4";
 
-        //region Attribution des adresses
+        //region Initialisation
         try {
+
+            //Init adresses
             localAdress =  InetAddress.getLocalHost();
             System.out.println("Votre adresse locale est " + localAdress);
             serveurAdress = InetAddress.getByName(nomAdresseServeur);
             System.out.println("L'adresse du serveur cible est " + serveurAdress);
+
+            //Creation socket Client
+            socketClient = new Socket(serveurAdress, 80);
+            System.out.println("Socket crée sur le port 80 !");
+
+            //Creation Streams
+            inClient = new DataInputStream(socketClient.getInputStream());
+            bufferIn = new BufferedInputStream(inClient);
+            outClient = new DataOutputStream(socketClient.getOutputStream());
+            bufferOut = new BufferedOutputStream(outClient);
         }
         catch ( UnknownHostException err ) {
             System.out.println("ERREUR : Adresses inconnus !");
             err.printStackTrace();
             return;
         }
-        //endregion
-
-        //region Création Socket client
-        try {
-            socketClient = new Socket(serveurAdress, 80);
-            System.out.println("Socket crée sur le port 80 !");
-        }
         catch (IOException err) {
-            System.out.println("ERREUR création Socket client !");
+            System.out.println("ERREUR création Socket client OU Streams !");
             err.printStackTrace();
             return;
         }
-        //endregion
+        //endregion Initialisation
 
-        //region get Input/Output Streams
-        try {
-            inClient = new DataInputStream(socketClient.getInputStream());
-            bufferIn = new BufferedInputStream(inClient);
-            outClient = new DataOutputStream(socketClient.getOutputStream());
-            bufferOut = new BufferedOutputStream(outClient);
-        }
-        catch (IOException err) {
-            System.out.println("Erreur création In/Out Streams !");
-            err.printStackTrace();
-            return;
-        }
-        //endregion
 
-        msg = "Salut?";
 
-        /*
         while (isConnected) {
 
+
+            //On écrit le msg à envoyer
+            System.out.println("Entrez un message...");
+            msg = sc.nextLine();
+
+            //System.out.println(msg);
+
+            //Envoie
             try {
-                outClient.writeBytes("GET " + msg + " HTTP/1.1\r\n\n");
+                outClient.writeBytes("GET " + msg + " HTTP/1.1\r\n\r\n");
                 outClient.flush();
                 System.out.println("Message envoyé!");
 
@@ -83,39 +82,23 @@ public class Client
                 err.printStackTrace();
             }
 
+            //Reception
+            try {
+                msg = inClient.readLine();
+                System.out.println("Réponse reçue : \'" + msg + "\'");
+            }
+            catch (IOException err) {
+                System.out.println("Erreur reception !");
+                err.printStackTrace();
+            }
 
-        } */
-
-
-        try {
-            outClient.writeBytes("GET " + msg + " HTTP/1.1\r\n\n");
-            outClient.flush();
-            System.out.println("Message envoyé!");
-
-        }
-        catch (IOException err) {
-            System.out.println("Erreur envoie GET");
-            err.printStackTrace();
-        }
-
-        //inClient.read();
-
-        //region boucle simple
-        /*
-        while (isConnected) {
-
-            System.out.println("Entrez un message...");
-            msg = sc.nextLine();
-
-            System.out.println(msg);
-
+            //Arrêt de la connexion si le msg = "quit"
             if ( msg.equals("quit")) {
                 isConnected = false;
             }
 
+
         }
-        */
-        //endregion
 
         //region Fermeture scanner et socket
         System.out.println("Arrêt du client...");
