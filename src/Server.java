9777e6861ede50468df08d0bc1c87da945434eac
Fab <fab.mckay@gmail.com>
2018-05-28 11:31:48 +0200

    Transfert fichier OK

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,4 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,12 +9,13 @@ public class Server
         try
         {
             ServerSocket serverSocket = new ServerSocket(80);
+
+
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("New connection accepted " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
             while(true)
             {
-                System.out.println("Waiting new Request ...");
-
                 //Reception de la requête
-                Socket clientSocket = serverSocket.accept();
                 InputStreamReader inputReader = new InputStreamReader(clientSocket.getInputStream());
                 BufferedReader reader = new BufferedReader(inputReader);
 
@@ -30,17 +30,48 @@ public class Server
                 {
                     System.out.println("C'est un GET");
                     System.out.println("Il veut " + splitRequest[1]);
+
+                    try {
+                        File fichier = new File(splitRequest[1]);
+
+                        if(fichier.canRead())
+                        {
+                            byte [] mybytearray  = new byte [(int)fichier.length()];
+
+                            FileInputStream fis = new FileInputStream(fichier);
+                            BufferedInputStream bis = new BufferedInputStream(fis);
+
+                            bis.read(mybytearray, 0, mybytearray.length);
+
+                            OutputStream os = clientSocket.getOutputStream();
+
+                            System.out.println("Sending " + splitRequest[1] + "(" + mybytearray.length + " bytes)");
+                            os.write(mybytearray, 0, mybytearray.length);
+                            os.flush();
+
+                            System.out.println("Done.");
+                        }
+                        else
+                        {
+                            String httpResponse = "HTTP/1.1 418 Error\r\nError 418 I'm a teapot\r\n" + "Fichier introuvable connard";
+                            clientSocket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+                            clientSocket.getOutputStream().flush();
+                        }
+
+                    }
+                    catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
+
+
+                    /*
+                    String httpResponse = "HTTP/1.1 200 OK\r\nError 418 I'm a teapot\r\n" + "Voilà ton fichier connard";
+                    clientSocket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+                    */
+
+                    System.out.println("Waiting new request ...");
                 }
-
-                String line = reader.readLine();
-
-                while(!line.isEmpty())
-                {
-                    line = reader.readLine();
-                }
-
-                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + "Reçu Reçu";
-                clientSocket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
             }
         }
         catch(Exception e)
